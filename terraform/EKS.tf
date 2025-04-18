4485146086d1c25f418542c17997811d956c745a
HenzelD <domhen2001@gmail.com>
2025-04-18 17:10:57 +0200

    no message

--- terraform/EKS.tf
+++ terraform/EKS.tf
@@ -2,11 +2,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-
-  iam_role_arn = module.eks_cluster_role.iam_role_arn
   cluster_name    = "Cluster"
   cluster_version = "1.31"
-  
+
+  authentication_mode = "API"
+
   access_entries = {
     jenkins = {
       principal_arn = "arn:aws:iam::713881808991:role/jenkins-ci-cd-role"
@@ -22,10 +22,8 @@ module "eks" {
     }
   }
 
-  # Optional
   cluster_endpoint_public_access = true
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
   cluster_compute_config = {
@@ -34,7 +32,12 @@ module "eks" {
   }
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = [module.vpc.private_subnets[0], module.vpc.public_subnets[0],module.vpc.private_subnets[1], module.vpc.public_subnets[1]]
+  subnet_ids = [
+    module.vpc.private_subnets[0],
+    module.vpc.public_subnets[0],
+    module.vpc.private_subnets[1],
+    module.vpc.public_subnets[1]
+  ]
 
   tags = {
     Environment = "dev"
